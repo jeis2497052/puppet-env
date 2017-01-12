537fbeb09a74fd459dcf1a6de0e6b9002350933a
Mike Terzo <mike@terzo.org>
2017-01-12 05:21:15 -0500

    50 clode cluster

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -1,6 +1,7 @@
-File { backup => false }
-
 node default {
+
+  hiera_include('classes')
+  
   file { '/tmp/puppet-in-docker':
     ensure  => present,
     content => 'This file is for demonstration purposes only',
